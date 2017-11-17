72a44e319c194698f4ca34248e1d43c4d1cf4b17
HariniParth <parth.harini@gmail.com>
2017-11-17 14:55:12 -0800

    Return a HashMap with ratio of meals

--- src/ratios/Ratios.java
+++ src/ratios/Ratios.java
@@ -5,12 +5,35 @@
  */
 package ratios;
 
+import java.util.*;
+
 /**
  *
  * @author harini-geek
  */
 public class Ratios {
 
+    private HashMap<Integer, Integer> mealRatios(int total, HashMap<Integer, Integer> mealPortions){
+        
+        // Key = Meal Identifier, Value = Meal Quantity
+        HashMap<Integer, Integer> meal = new HashMap<>();
+        
+        int singlePortion;
+        int totalPortions = 0;
+        // Iterate through the HashMap to determine the quantity in 1 portion of the meal
+        for(Map.Entry<Integer, Integer> entrySet : mealPortions.entrySet()){
+            totalPortions += entrySet.getValue();
+        }
+        singlePortion = total / totalPortions;
+        
+        // Return a HashMap with Keys being meal identifiers and values being meal quantities.
+        for(Map.Entry<Integer, Integer> entrySet : mealPortions.entrySet()){
+            meal.put(entrySet.getKey(), singlePortion * entrySet.getValue());
+        }
+        
+        return meal;
+    }
+    
     /**
      * @param args the command line arguments
      */
